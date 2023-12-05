854dbde08c9dbf03ea5ebc491996b509cac0264c
Kilyong Jeong <uzoolove@gmail.com>
2023-12-05 18:01:47 +0900

    ✨ 판매자에게 주문한 모든 주문 목록 조회

--- api/routes/seller/orders.js
+++ api/routes/seller/orders.js
@@ -4,15 +4,26 @@ import { query, body } from 'express-validator';
 import logger from '#utils/logger.js';
 import validator from '#middlewares/validator.js';
 import model from '#models/user/order.model.js';
+import sellerModel from '#models/seller/order.model.js';
 import _ from 'lodash';
 import moment from 'moment';
 
 const router = express.Router();
 
-// 주문 상태 수정
-router.patch('/:_id', validator.checkResult, async function(req, res, next) {
+// 판매자의 모든 주문 내역 조회
+router.get('/', async function(req, res, next) {
   try{
-    logger.trace(req.query);
+    const item = await sellerModel.findBy({seller_id: req.user._id, query: {}, sortBy: {_id: -1}});
+    res.json({ ok: 1, item });
+
+  }catch(err){
+    next(err);
+  }
+});
+
+// 주문 상태 수정
+router.patch('/:_id', async function(req, res, next) {
+  try{
     const _id = Number(req.params._id);
     const product_id = req.body.product_id;
     const order = await model.findById(_id);
