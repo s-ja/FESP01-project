3b455b6263b1b315c665d7695b2531ec637184a5
an seungji <ansj8777@gmail.com>
2024-05-01 15:41:53 +0900

    액세스 토큰 만료 시간 복구 : 3분 -> 2시간

--- api/config/index.js
+++ api/config/index.js
@@ -23,11 +23,10 @@ export const jwt = {
     secretKey: "ShoppingAccessToken", // 암호키
     options: {
       algorithm: "HS256", // 대칭키 방식
-      // expiresIn: "2h", // 2시간
+      expiresIn: "2h", // 2시간
       // expiresIn: '10m', // 10분
-      expiresIn: "3m", // 3분
+      // expiresIn: "3m", // 3분
       //   expiresIn: '10s',  // 10초
-      //   expiresIn: '1s',  // 1초
       issuer: "FESP01", // 발행자
     },
   },
